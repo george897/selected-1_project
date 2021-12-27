c6fa44318d95055cfc7b72c9efff1c0a5ddba59b
george897 <76627230+george897@users.noreply.github.com>
2021-12-27 15:11:10 +0200

    george897

--- image dataset/SVM/main.py
+++ image dataset/SVM/main.py
@@ -30,8 +30,8 @@ from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
 from sklearn.datasets import load_digits
 from sklearn.naive_bayes import GaussianNB
-
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 
 
 
@@ -95,6 +95,7 @@ plt.figure(figsize=(10,7))
 sns.heatmap(cm, annot=True, fmt='d')
 plt.xlabel('predicted')
 plt.ylabel('Truth')
+print(cm)
 
 
 
@@ -123,114 +124,40 @@ accuracy_score(y_test, prediction)
 
 
 
-#learning curve
-def plot_learning_curve(
-    estimator,
-    title,
-    X,
-    y,
-    axes=None,
-    ylim=None,
-    cv=None,
-    n_jobs=None,
-    train_sizes=np.linspace(0.1, 1.0, 5),
-):
-    
-    if axes is None:
-        _, axes = plt.subplots(1, 3, figsize=(20, 5))
-
-    axes[0].set_title(title)
-    if ylim is not None:
-        axes[0].set_ylim(*ylim)
-    axes[0].set_xlabel("Training examples")
-    axes[0].set_ylabel("Score")
-
-    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
-        estimator,
-        X,
-        y,
-        cv=cv,
-        n_jobs=n_jobs,
-        train_sizes=train_sizes,
-        return_times=True,
-    )
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    fit_times_mean = np.mean(fit_times, axis=1)
-    fit_times_std = np.std(fit_times, axis=1)
-
-    # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(
-        train_sizes,
-        train_scores_mean - train_scores_std,
-        train_scores_mean + train_scores_std,
-        alpha=0.1,
-        color="r",
-    )
-    axes[0].fill_between(
-        train_sizes,
-        test_scores_mean - test_scores_std,
-        test_scores_mean + test_scores_std,
-        alpha=0.1,
-        color="g",
-    )
-    axes[0].plot(
-        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
-    )
-    axes[0].plot(
-        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
-    )
-    axes[0].legend(loc="best")
-
-    # Plot n_samples vs fit_times
-    axes[1].grid()
-    axes[1].plot(train_sizes, fit_times_mean, "o-")
-    axes[1].fill_between(
-        train_sizes,
-        fit_times_mean - fit_times_std,
-        fit_times_mean + fit_times_std,
-        alpha=0.1,
-    )
-    axes[1].set_xlabel("Training examples")
-    axes[1].set_ylabel("fit_times")
-    axes[1].set_title("Scalability of the model")
-
-    # Plot fit_time vs score
-    fit_time_argsort = fit_times_mean.argsort()
-    fit_time_sorted = fit_times_mean[fit_time_argsort]
-    test_scores_mean_sorted = test_scores_mean[fit_time_argsort]
-    test_scores_std_sorted = test_scores_std[fit_time_argsort]
-    axes[2].grid()
-    axes[2].plot(fit_time_sorted, test_scores_mean_sorted, "o-")
-    axes[2].fill_between(
-        fit_time_sorted,
-        test_scores_mean_sorted - test_scores_std_sorted,
-        test_scores_mean_sorted + test_scores_std_sorted,
-        alpha=0.1,
-    )
-    axes[2].set_xlabel("fit_times")
-    axes[2].set_ylabel("Score")
-    axes[2].set_title("Performance of the model")
-
-    return plt
+# learning curve
 
 
-fig, axes = plt.subplots(3, 2, figsize=(10, 15))
 
-X, y = load_digits(return_X_y=True)
-
-title = "Learning Curves (Naive Bayes)"
-# Cross validation with 50 iterations to get smoother mean test and train
-# score curves, each time with 20% data randomly selected as a validation set.
-cv = ShuffleSplit(n_splits=50, test_size=0.2, random_state=0)
-
-estimator = GaussianNB()
-plot_learning_curve(
-    estimator, title, X, y, axes=axes[:, 0], ylim=(0.7, 1.01), cv=cv, n_jobs=4
-)
-
-
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_digits
+from sklearn.model_selection import learning_curve
+  
+# Load data set
+dataset = load_digits()
+  
+# X contains data and y contains labels
+X, y = dataset.data, dataset.target
+  
+# Obtain scores from learning curve function
+# cv is the number of folds while performing Cross Validation
+sizes, training_scores, testing_scores = learning_curve(KNeighborsClassifier(), X, y, cv=10, scoring='accuracy', train_sizes=np.linspace(0.01, 1.0, 50))
+  
+# Mean and Standard Deviation of training scores
+mean_training = np.mean(training_scores, axis=1)
+Standard_Deviation_training = np.std(training_scores, axis=1)
+  
+# Mean and Standard Deviation of testing scores
+mean_testing = np.mean(testing_scores, axis=1)
+Standard_Deviation_testing = np.std(testing_scores, axis=1)
+  
+# dotted blue line is for training scores and green line is for cross-validation score
+plt.plot(sizes, mean_training, '--', color="b",  label="Training score")
+plt.plot(sizes, mean_testing, color="g", label="Cross-validation score")
+  
+# Drawing plot
+plt.title("LEARNING CURVE FOR KNN Classifier")
+plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score"), plt.legend(loc="best")
+plt.tight_layout()
 plt.show()
